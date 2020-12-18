45cd52f8c1064a71ab5772bb6c2fb6f773e50648
KalluruS <>
2020-12-18 08:40:41 +0530

    Update terraform/

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -5,3 +5,19 @@ variable "a" {
 output "a" {
   value = var.a
 }
+
+## THese variables are three types
+
+variable "normal" {
+  default = 10
+}
+
+variable "list" {
+  default = [ 100, "abc", true ]
+}
+
+## In one list variable you can give multiple values which is of different data types.
+
+output "list_1" {
+  value = var.list[0]
+}
