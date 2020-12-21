d71da14e197bc852cc17a28f8a2e293fc21f6245
KalluruS <>
2020-12-21 07:52:16 +0530

    Update terraform/

--- 06-modules/module1/main.tf
+++ 06-modules/module1/main.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "sample" {
   ami                       = "ami-052ed3344670027b3"
-  instance_type             = "t3.micro"
+  instance_type             = var.INSTANCE_TYPE
 }
 
 resource "aws_instance" "sample1" {
   ami                       = "ami-052ed3344670027b3"
   instance_type             = "t3.micro"
 }
+
+variable "INSTANCE_TYPE" {}
