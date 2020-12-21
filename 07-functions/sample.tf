f15a30084178fc0dcab3f6bb6a3bd93298966cea
KalluruS <>
2020-12-21 08:31:47 +0530

    Update terraform/

---
+++ 07-functions/sample.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "sample" {
+  ami                       = "ami-052ed3344670027b3"
+  instance_type             = "t3.micro"
+  tags                      = {
+        Name                = upper(var.name)
+  }
+
+}
+
+variable "name" {
+  default = "sample"
+}
