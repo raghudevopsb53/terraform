9d8fbeda14d8ea56767ddf97bc002ea8e7045340
KalluruS <>
2020-12-21 08:36:05 +0530

    Update terraform/

--- 07-functions/sample.tf
+++ 07-functions/sample.tf
@@ -10,3 +10,13 @@ resource "aws_instance" "sample" {
 variable "name" {
   default = "sample"
 }
+
+variable "image_id" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+
+  validation {
+    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+  }
+}
